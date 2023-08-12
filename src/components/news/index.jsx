7c49dea839966226780471b05a738583aabc68a2
Keval Shetta <shetta20keval@gmail.com>
2023-08-11 22:20:57 -0400

    Bookmarked_stored
    Profile
    Bookmark id is stored

--- src/components/news/index.jsx
+++ src/components/news/index.jsx
@@ -1,7 +1,37 @@
 import React from "react";
-import { StyleSheet, View, Text, Image } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import { StyleSheet, View, Text, Image, TouchableOpacity } from "react-native";
+import { getFirestore, collection, addDoc } from "firebase/firestore";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
+import firebase from "firebase/app";
+import db from "../../database/config";
 
 function NewsItem({ item }) {
+  const firestore = getFirestore();
+  const dbCollectionRef = collection(firestore, "bookmarks"); // Corrected this line
+
+  const bookMarkArticle = async () => {
+    try {
+      await addDoc(dbCollectionRef, {
+        newsItemId: item.id,
+        // You can add more fields as needed
+      });
+
+      console.log("News item bookmarked!");
+    } catch (error) {
+      console.error("Error bookmarking news item: ", error);
+    }
+  };
+
+  if (user === null) {
+    return (<Text>Loading...</Text>);
+  }
+
+  if (user ==!null) {
+    bookMarkArticle();
+  }
+
   return (
     <View style={styles.content} key={item.id}>
       <Image
@@ -11,6 +41,9 @@ function NewsItem({ item }) {
       <View style={styles.mainText}>
         <Text>{item.title}</Text>
       </View>
+      <TouchableOpacity onPress={bookMarkArticle}>
+        <MaterialIcons name="bookmark" size={24} color="black" />
+      </TouchableOpacity>
     </View>
   );
 }
